95af19c2bf9d1de38b7528b42093c9e686cc1471
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-19 15:10:20 -0400

    up to date as of class

--- jshotel.js
+++ jshotel.js
@@ -1,6 +1,6 @@
 
 
-document.getElementsByClassName("slogan")[0].innerText = "SLEEP FOREVER"
+document.getElementsByClassName("slogan")[0].innerText = "plz let me sleep no code"
 document.getElementById("101").getElementsByTagName("a")[0].innerHTML = "210";
 document.getElementById("102").getElementsByTagName("a")[0].innerHTML = "211";
 document.getElementById("103").getElementsByTagName("a")[0].innerHTML = "212";
