79139e90679261f9e0f65a7349c0ff238a3e1543
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-19 15:08:58 -0400

    buttons are formatted correctly

--- jshotel.js
+++ jshotel.js
@@ -1,3 +1,6 @@
-alert("Welcome!");
 
 
+document.getElementsByClassName("slogan")[0].innerText = "SLEEP FOREVER"
+document.getElementById("101").getElementsByTagName("a")[0].innerHTML = "210";
+document.getElementById("102").getElementsByTagName("a")[0].innerHTML = "211";
+document.getElementById("103").getElementsByTagName("a")[0].innerHTML = "212";
