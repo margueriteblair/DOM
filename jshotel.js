5401ed16d3b089de70530f31c113e2cf4bec7e1e
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-19 14:52:46 -0400

    successfully added JS to edit room and slogan

--- jshotel.js
+++ jshotel.js
@@ -1,7 +1,3 @@
-document.getElementsByClassName("slogan")[0].innerText = "SLEEP FOREVER"
-document.getElementById("101").innertext = "210";
-document.getElementById("102").innertext = "211";
-document.getElementById("103").innertext = "212";
-document.getElementById("103").innertext = "";
-document.getElementById("103").innertext = "";
+alert("Welcome!");
+
 
