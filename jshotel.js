58b9c49f0f24bc3312e673c82a4d9380086606a7
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-20 13:55:02 -0400

    basic table with CSS styling

--- jshotel.js
+++ jshotel.js
@@ -1,5 +1,3 @@
-
-
 document.getElementsByClassName("slogan")[0].innerText = "plz let me sleep no code"
 document.getElementById("101").getElementsByTagName("a")[0].innerHTML = "210";
 document.getElementById("102").getElementsByTagName("a")[0].innerHTML = "211";
