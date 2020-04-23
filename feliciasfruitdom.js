07f3326fde4cb4d8396c91787347d8700640f12c
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-22 21:30:26 -0400

    created CSS file for styling

--- feliciasfruitdom.js
+++ feliciasfruitdom.js
@@ -1,1 +1,22 @@
-document.getElementsByClassName("slogan").innerText = "Buy Felicia!"
+document.getElementById("name").innerText = "Welcome to Felicia's Fruits (Online for COVID!)";
+document.getElementById("slogan").innerText = "Buy Felicia!";
+document.getElementById("pickup").innerText = "We do curbside pickup and delivery!"
+
+let fruitsArray = ["Apples", "Bananas", "Peaches", "Mangos", "Strawberries", "Blackberries"];
+let quantities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+let dropDownTag = "<select>"
+for (let i = 0; i < fruitsArray.length; i++){
+    dropDownTag = dropDownTag + `<option value = "${fruitsArray[i]}">${fruitsArray[i]}</option>`
+}
+dropDownTag + "</select>"
+
+dropDownTag2 = "<select>"
+for (let i = 0; i < quantities.length; i++){
+    dropDownTag2 = dropDownTag2 + `<option value = "${quantities[i]}">${quantities[i]}</option>`
+}
+dropDownTag2 = dropDownTag2 + "</select>"
+
+
+document.getElementById("fruitsavail").innerHTML = dropDownTag;
+document.getElementById("quantities").innerHTML = dropDownTag2;
