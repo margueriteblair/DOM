bb7f77151340a26a769dd0cff5684147e70f54ee
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-24 01:55:14 -0400

    new DOM hotel, outbook function

---
+++ domhotel03.js
@@ -0,0 +1,26 @@
+document.getElementById("title").innerText = "WELCOME TO THE NEW AND IMPROVED DOM HOTEL!"
+document.getElementById("slogan").innerText = "Come and Git It!"
+
+let domhotel = {
+     availableRooms: [["101", "113", "120"], ["201"], ["301", "302", "303", "304"],["402", "404"]],
+     bookedRooms: [[],[],[],[]],
+     roomTypes: ["Single", "Double", "Queen", "King"],
+
+     bookRoom: function() {
+
+     },
+
+     updateTheInventoryList: function() {
+         let dropDown = "<select>"
+         for (let i = 0; i < this.availableRooms.length; i++) {
+             for(let j = 0; j < this.availableRooms[i].length; j++) {
+                 dropDown = dropDown + `<option value = "${this.availableRooms[i][j]}">${this.availableRooms[i][j]}</option>`
+             }
+         }
+            dropDown = dropDown + "</select>"
+            document.getElementById("dropdown").innerHTML = dropDown;
+     }
+
+}
+
+domhotel.updateTheInventoryList();
