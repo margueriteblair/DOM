d2fae6d2489c43db500b72bc767a23660b6100b9
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-26 18:28:15 -0400

    functioning loop to display available rooms.

--- jshotel04.js
+++ jshotel04.js
@@ -4,3 +4,12 @@ document.getElementById("slogan").innerText = "Code All Day, Code All Night";
 let availableRooms = [[101, 102, 103, 104], [202, 204, 207], [301, 303],[401, 402, 403, 404]];
 let bookedRooms = [[],[],[],[]];
 let roomTypes = ["Single", "Double", "Queen", "King"];
+
+let dropDown = "<select>";
+for (let i = 0; i < availableRooms.length; i++) {
+    for (let j = 0; j < availableRooms[i].length; j++) {
+        dropDown = dropDown + `<option value = ${availableRooms[i][j]}>${availableRooms[i][j]}</option>`
+    }
+}
+        dropDown = dropDown + "<select>"
+        document.getElementById("dropdown").innerHTML = dropDown;
