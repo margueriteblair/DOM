1b13c532bb5ccd1d6ecd9129663b958ad1513956
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-29 20:42:53 -0400

    unbook a room function with onclick

--- domgrouphotel.js
+++ domgrouphotel.js
@@ -14,14 +14,33 @@ let hotel = {
     },
     bookSelectedRoom: function() {
         let selectedAvailableRoom = parseInt(document.getElementById("availableSelect").value);
-        console.log(selectedAvailableRoom);
+        // console.log(selectedAvailableRoom);
         for (let i = 0; i < this.availableRooms.length; i++) {
             for (let j= 0; j < this.availableRooms[i].length; j++) {
                 if (selectedAvailableRoom == this.availableRooms[i][j]) {
                     console.log(typeof(selectedAvailableRoom));
                     console.log(typeof(this.availableRooms[i][j]));
+                    console.log(selectedAvailableRoom);
+                    console.log(this.availableRooms[i][j]);
                     this.bookedRooms[i].push(this.availableRooms[i].splice(this.availableRooms[i].indexOf(selectedAvailableRoom),1))
+                    // this.bookedRooms[i].push(this.availableRooms[i][j]);
                    
+                }
+            }
+        }
+        this.updateAvailableRooms();
+        this.updateBookedRooms();
+    },
+
+    unbookSelectedRoom: function() {
+        let selectedUnavailableRoom = parseInt(document.getElementById("unavailableSelect").value);
+        console.log(selectedUnavailableRoom);
+        for (let i = 0; i < this.bookedRooms.length; i++) {
+            for (let j = 0; j < this.bookedRooms[i].length; j++) {
+                if (selectedUnavailableRoom == this.bookedRooms[i][j]) {
+                    console.log(typeof(selectedUnavailableRoom));
+                    this.bookedRooms[i].splice(this.bookedRooms[i].indexOf(selectedUnavailableRoom),1);
+                    this.availableRooms[i].push(this.bookedRooms[i][j]);
                 }
             }
         }
@@ -41,7 +60,7 @@ let hotel = {
 
     },
     updateBookedRooms: function() {
-        let dropdown = `<select>`
+        let dropdown = `<select id = "unavailableSelect">`
         for (let i = 0; i < this.bookedRooms.length; i++) {
             for (let j= 0; j < this.bookedRooms[i].length; j++) {
                 dropdown = dropdown + `<option value = "${this.bookedRooms[i][j]}">${this.bookedRooms[i][j]}</option>`
